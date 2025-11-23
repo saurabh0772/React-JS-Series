14c68817e28d63ac1e39c2524fb07ed409316ca4
Saurabh <hackeryazama0772@gmail.com>
2025-11-23 20:30:59 +0530

    app

--- 10-todoList/src/App.jsx
+++ 10-todoList/src/App.jsx
@@ -1,37 +1,71 @@
-import { useState } from 'react'
-import './contexts/index'
-import { TodoProvider } from './contexts/index'
+import { useState, useEffect } from 'react'
+import {TodoProvider} from './contexts'
+import './App.css'
+import TodoForm from './components/TodoForm'
+import TodoItem from './components/TodoItem'
 
 function App() {
-  
-  // const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState([])
 
-  const addTask = (e) => {
-    e.preventDefault();
+  const addTodo = (todo) => {
+    setTodos((prev) => [{id: Date.now(), ...todo}, ...prev] )
   }
 
+  const updateTodo = (id, todo) => {
+    setTodos((prev) => prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo )))
+
+    
+  }
+
+  const deleteTodo = (id) => {
+    setTodos((prev) => prev.filter((todo) => todo.id !== id))
+  }
+
+  const toggleComplete = (id) => {
+    //console.log(id);
+    setTodos((prev) => 
+    prev.map((prevTodo) => 
+      prevTodo.id === id ? { ...prevTodo, 
+        completed: !prevTodo.completed } : prevTodo))
+  }
+
+  useEffect(() => {
+    const todos = JSON.parse(localStorage.getItem("todos"))
+
+    if (todos && todos.length > 0) {
+      setTodos(todos)
+    }
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos))
+  }, [todos])
+  
+
+
+
   return (
-    <>
-      <div className='w-full h-screen bg-black'>
-        <div className='flex flex-col w-100 mx-auto pt-20 pb-20 border border-white'>
-          <div className='flex '>
-            <form className='mx-auto border border-green-500'>
-                <input type="text" placeholder='Enter Task' className='bg-white outline-none rounded-l-lg p-2 w-87 border border-red-400'/>
-                <button onSubmit={addTask} className='bg-blue-400 p-2 rounded-r-lg cursor-pointer text-white'>Add</button>
-            </form>
-          </div>
-
-        <div className='flex justify-center border border-white'>
-            <div className='text-white'>
-              <input type="text" className='bg-white'/>
-              <button>Edit</button>
-              <button>Delete</button>
+    <TodoProvider value={{todos, addTodo, updateTodo, deleteTodo, toggleComplete}}>
+      <div className="bg-[#172842] min-h-screen py-8">
+                <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+                    <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
+                    <div className="mb-4">
+                        {/* Todo form goes here */} 
+                        <TodoForm />
+                    </div>
+                    <div className="flex flex-wrap gap-y-3">
+                        {/*Loop and Add TodoItem here */}
+                        {todos.map((todo) => (
+                          <div key={todo.id}
+                          className='w-full'
+                          >
+                            <TodoItem todo={todo} />
+                          </div>
+                        ))}
+                    </div>
+                </div>
             </div>
-        </div>
-        </div>
-
-      </div>
-    </>
+    </TodoProvider>
   )
 }
 
